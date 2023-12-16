f63d7106073fb4d09e0a2ca6ded5e638d8e0ec1b
ChrisRDavila <crossdavila@gmail.com>
2023-12-15 17:04:17 -0800

    Good Fail for test 2 in orderListReducer

--- src/__tests__/reducers/order-list-reducer.test.js
+++ src/__tests__/reducers/order-list-reducer.test.js
@@ -1,7 +1,44 @@
 import orderListReducer from '../../reducers/order-list-reducer';
 
 describe('orderListReducer', () => {
-  test('Should return default state if there is no action type passed into the reducer', () => {
+
+  let action;
+  const orderData = {
+    names: 'Red Eye',
+    origin: 'Columbian',
+    price: 20,
+    roast: 'dark',
+    bags: 1,
+    id: 1
+  };
+
+  test('#1) Should return default state if there is no action type passed into the reducer', () => {
     expect(orderListReducer({}, { type: null })).toEqual({});
   });
+
+  test('#2) Should successfully add new order data to mainOrderList', () => {
+    const { names, origin, price, roast, bags, id } = orderData;
+    action = {
+      type: 'ADD_ORDER',
+      names: names,
+      origin: origin,
+      price: price,
+      roast: roast,
+      bags: bags,
+      id: id
+    };
+
+    expect(orderListReducer({}, action)).toEqual({
+      [id] : {
+        names: names,
+        origin: origin,
+        price: price,
+        roast: roast,
+        bags: bags,
+        id: id
+      }
+    });
+  });
+
+
 });
